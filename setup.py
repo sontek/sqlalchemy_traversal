0a27085d34c55ffe9dd96e33688dfcdc88062c24
John Anderson <sontek@gmail.com>
2013-01-28 10:50:04 -0300

    released new version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ requires = [
 
 setup(
     name = name
-    , version='0.5.1'
+    , version='0.5.2'
     , url='http://github.com/eventray/' + name
     , author='John Anderson'
     , author_email='sontek@gmail.com'
